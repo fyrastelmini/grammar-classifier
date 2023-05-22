c804b46ad4df219f34f3f3e0ec73a6158e614334
Fyras Telmini <mohamed.telmini@ensae.fr>
2023-05-22 06:06:51 +0200

    removed tokenizer

--- data/dataloader.py
+++ data/dataloader.py
@@ -1,9 +1,8 @@
-from transformers import BertTokenizer
 import pandas as pd
 import numpy as np
 
-def make_dataset(df,padding_size=28):
-	tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+def make_dataset(df,tokenizer,padding_size=28):
+
 	dataset_df=pd.DataFrame()
 	dataset_df[0]=df[0].apply(lambda x: tokenizer(x, padding='max_length',max_length=padding_size)["input_ids"])
 	dataset_df[1]=df[1].apply(lambda x: np.array(float(x)))
